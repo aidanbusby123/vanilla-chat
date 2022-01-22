a778b02ca021ce3a5d4d2e9f04a237cf2e6f74a2
Nate <nategamedevelopmentacc@gmail.com>
2022-01-22 14:09:58 -0800

    add cmake

--- src/main.c
+++ src/main.c
@@ -1,11 +1,30 @@
+/*    
+    This is the main file for vanilla chat, this is where the program starts
+    Copyright (C) 2021  Aidan Busby
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, Ma 02110-1301 USA.
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
 #include<termios.h>
 #include<pthread.h>
-#include"../include/client.h"
-#include"../include/server.h"
+#include "../include/client.h"
+#include "../include/server.h"
 
 char buf[64];
 int x = 0;
