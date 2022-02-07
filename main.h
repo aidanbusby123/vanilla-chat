3518948d034ae7f6b5b9938145bc5db2732e0940
Aidan <aidanbusby123@gmail.com>
2022-02-06 20:41:15 -0800

    test

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef main
+#define main
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<termios.h>
+#include<pthread.h>
+#include"server.h"
+#include"client.h"
+#include"terminalmode.h"
+
+#endif
+
